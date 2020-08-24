230be1b418053a852a1d12ddfa7886a081a8a06e
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-24 10:18:03 -0700

    added dry data

---
+++ data/yelp.js
@@ -0,0 +1,26 @@
+module.exports = [
+  { 
+    category: 'hotel',
+    business_type: 'hotel',
+    business_name: 'The Nines',
+    review: 'Probably the most disgusting thing was the bathroom it self. Beautiful Carrera marble and stainless fixtures surround a dingy, filthy tub',
+    rating: 1,
+    image_url: 'https://placekitten.com/300/300'
+  },
+  {
+    category: 'buffet',
+    business_type: 'restaurant',
+    business_name: 'Chicken Connection',
+    review: 'The dumpsters out back probably have better options.',
+    rating: 2,
+    image_url: 'https://placekitten.com/300/300'
+  },
+  {
+    category: 'cafe',
+    business_type: 'restaurant',
+    business_name: 'Milton/s Coffee Co',
+    review: 'I ordered a mocha and it was so burnt tasting I had to toss it.',
+    rating: 1,
+    image_url: 'https://placekitten.com/300/300'
+  }
+];
