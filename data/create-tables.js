01be6c7933a557fb8cb6875995c615e7a134ecab
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-27 21:21:56 -0700

    removed commented out code

--- data/create-tables.js
+++ data/create-tables.js
@@ -1,16 +1,13 @@
 const client = require('../lib/client');
 const { getEmoji } = require('../lib/emoji.js');
 
-// async/await needs to run in a function
 run();
 
 async function run() {
 
   try {
-    // initiate connecting to db
     await client.connect();
 
-    // run a query to create tables
     await client.query(`
                 CREATE TABLE users (
                     id SERIAL PRIMARY KEY,
@@ -29,16 +26,12 @@ async function run() {
                     owner_id INTEGER NOT NULL REFERENCES users(id)
                     );
                 `);
-
-
     console.log('create tables complete', getEmoji(), getEmoji(), getEmoji());
   }
   catch(err) {
-    // problem? let's see the error...
     console.log(err);
   }
   finally {
-    // success or failure, need to close the db connection
     client.end();
   }
 
